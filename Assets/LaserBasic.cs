cf470a1504cff2a679fad63e571b0193bc3d3b68
Berler666 <destroyed76@hotmail.com>
2017-04-01 18:14:06 +1100

    laser now destroys when hitting object tagged as enemy. Also destroy after 3 seconds

---
+++ Assets/LaserBasic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBasic : MonoBehaviour {
+
+    Unit enemy;
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, 3);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider enemy)
+    {
+        if(enemy.tag == "enemy")
+        {
+            HitTarget();
+        }
+    }
+
+    void HitTarget()
+    {
+        Destroy(gameObject);
+    }
+}
