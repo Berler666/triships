f5bc6a73f40407c6052a217d5dc76a7a66ed0a5c
Berler666 <destroyed76@hotmail.com>
2017-05-14 17:32:05 +1000

    project clean up

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour {
+
+    bool isZoomed = true;
+
+    GameObject cameraControl;
+
+	// Use this for initialization
+	void Start () {
+
+       cameraControl = GameObject.Find("Main Camera");
+       
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Zoom()
+    {
+        if(isZoomed == false)
+        {
+            
+            
+        }
+    }
+}
---
+++ Assets/Scripts/playerMotherShip.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMotherShip : MonoBehaviour {
+
+    public GameObject x1;
+
+    public GameObject VHScamera;
+   
+
+    public GameObject mothershipMenu;
+
+    public float spawntime = 5f;
+
+    bool isPlayer = true;
+  
+
+    
+
+	void Awake () {
+
+        StartCoroutine(SpawnShip());
+
+        VHScamera.SetActive(false);
+
+       if(gameObject.GetComponent<Unit>().teamNumber != 1)
+        {
+            isPlayer = false;
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator SpawnShip()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawntime);
+            Debug.Log("spawning ship");
+            Vector3 position = transform.position + new Vector3(Random.Range(-5f, 5f), 0.5f, Random.Range(-5f, 5f));
+            Instantiate(x1, position, Quaternion.identity);
+        }
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (isPlayer == true)
+        {
+            Debug.Log("paused");
+            VHScamera.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
