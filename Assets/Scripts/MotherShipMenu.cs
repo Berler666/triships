0aa0daecd51170a6515feab06217f25a75312c5a
Berler666 <destroyed76@hotmail.com>
2017-05-14 19:55:14 +1000

    Mothership resource bars now reflect resource values

--- Assets/Scripts/MotherShipMenu.cs
+++ Assets/Scripts/MotherShipMenu.cs
@@ -19,15 +19,22 @@ public class MotherShipMenu : MonoBehaviour {
     public GameObject scrapText;
     public GameObject unitsText;
 
+    public Image healthBar;
+    public Image powerBar;
+    public Image ammoBar;
+    public Image scrapBar;
+    public Image unitsBar;
+
+
     
-    GameObject mothership;
+    private playerMotherShip mothership;
     float health;
     float maxHealth;
 
 	// Use this for initialization
 	void Start () {
 
-        mothership = GameObject.Find("PlayerMothership");
+        mothership = GameObject.Find("PlayerMothership").GetComponent<playerMotherShip>();
         if (!mothership)
             Debug.Log("Can not find player mothership");
         
@@ -46,11 +53,23 @@ public class MotherShipMenu : MonoBehaviour {
         maxHealth = mothership.GetComponent<Unit>().maxHealth;
 
         healthText.GetComponent<Text>().text = (health + "/" + maxHealth);
-        powerText.GetComponent<Text>().text = (mothership.GetComponent<playerMotherShip>().power + "/" + mothership.GetComponent<playerMotherShip>().maxPower);
-        ammoText.GetComponent<Text>().text = (mothership.GetComponent<playerMotherShip>().ammo + "/" + mothership.GetComponent<playerMotherShip>().maxAmmo);
-        scrapText.GetComponent<Text>().text = (mothership.GetComponent<playerMotherShip>().scrap + "/" + mothership.GetComponent<playerMotherShip>().maxScrap);
-        unitsText.GetComponent<Text>().text = (mothership.GetComponent<playerMotherShip>().units + "/" + mothership.GetComponent<playerMotherShip>().maxUnits);
+        powerText.GetComponent<Text>().text = (mothership.power + "/" + mothership.maxPower);
+        ammoText.GetComponent<Text>().text = (mothership.ammo + "/" + mothership.maxAmmo);
+        scrapText.GetComponent<Text>().text = (mothership.scrap + "/" + mothership.maxScrap);
+        unitsText.GetComponent<Text>().text = (mothership.units + "/" + mothership.maxUnits);
 
+        healthBar.fillAmount = Map(health,0,maxHealth,0,1);
+        powerBar.fillAmount = Map(mothership.power, 0, mothership.maxPower, 0, 1);
+        ammoBar.fillAmount = Map(mothership.ammo, 0, mothership.maxAmmo, 0, 1);
+        scrapBar.fillAmount = Map(mothership.scrap, 0, mothership.maxScrap, 0, 1);
+        unitsBar.fillAmount = Map(mothership.units, 0, mothership.maxUnits, 0, 1);
+        
+
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
     public void BackButton()
