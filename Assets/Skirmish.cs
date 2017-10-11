480271acfa4925fca183d53f1365c175663b5cef
Berler666 <destroyed76@hotmail.com>
2017-10-11 14:04:14 +1100

    started skirmish, music slider, fixes

---
+++ Assets/Skirmish.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Skirmish : MonoBehaviour {
+
+    public Dropdown mapDropdown;
+    public string[] maps;
+
+	// Use this for initialization
+	void Start () {
+      
+
+        foreach (string scene in maps )
+        {
+            mapDropdown.options.Add(new Dropdown.OptionData(scene.ToString()));
+        }
+		
+	}
+
+  
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
